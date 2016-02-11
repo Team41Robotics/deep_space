33fe7941709b0ec2790b63c9715447be00aa6810
Prateek Humane <prateek.humane@gmail.com>
2016-02-11 11:42:54 -0500

    updated hostname

--- systemd/files/deep_space.sh
+++ systemd/files/deep_space.sh
@@ -5,10 +5,10 @@ source /home/nvidia/catkin_ws/devel/setup.bash
 hostname=$(ifconfig eth0 | grep 'inet addr' | cut -d':' -f2 | awk '{print $1}')
 if [ -z "$hostname" ]; then
 	export ROS_HOSTNAME=10.0.41.101
-	export ROS_MASTER_URI="http://$ROS_HOSTNAME:11311"
+	export ROS_MASTER_URI="http://$ROS_HOSTNAME:5809"
 else
 	export ROS_HOSTNAME=$hostname
-	export ROS_MASTER_URI="http://$ROS_HOSTNAME:11311"
+	export ROS_MASTER_URI="http://$ROS_HOSTNAME:5809"
 fi
 
 roslaunch deep_space all.launch
