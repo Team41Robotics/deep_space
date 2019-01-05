527be720145a56cac94e17e22f27830a8706f250
Prateek Humane <prateek.humane@gmail.com>
2019-01-05 13:59:58 -0500

    initial import
    Boiler plate files for lidar processing. Just putting
    files in place so they build. Lidar just does very
    simplistic processing that will be evolved later.

---
+++ setup.py
@@ -0,0 +1,16 @@
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+def read(fname):
+    return open(fname).read()
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=['lidar_processor'],
+    package_dir={'': 'src'},
+    platforms=['ROS/t_41_2019']
+)
+
+setup(**setup_args)
---
+++ src/lidar_processor/get_lidar.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import rospy
+from networktables import NetworkTables
+from sensor_msgs.msg import LaserScan
+
+sd = None
+
+def callback(msg):
+	print('lidar front')
+	print(msg.ranges[0])
+	print('min lidar index and distance')
+	index = 0
+	m = max(msg.ranges) # Minimum
+	for i in range(len(msg.ranges)):
+		if msg.ranges[i] < m and msg.ranges[i] > 0:
+			m = msg.ranges[i]
+			index = i
+	print('{}:\t {}'.format(index,m))
+	sd.putNumber('testnum',index)
+
+def getLidarData():
+	global sd
+	NetworkTables.initialize(server='10.0.41.62')
+	sd = NetworkTables.getTable("SmartDashboard")
+	rospy.init_node('getLidar',anonymous = True)
+	rospy.Subscriber('scan',LaserScan,callback)
+
+def main():
+	rospy.init_node('lidar_processor')
+	getLidarData()
+	rospy.spin()
+
+if __name__=='__main__':
+	try:
+		main()
+	except rospy.ROSInterruptException:
+		rospy.loginfo('interrupt caught')
