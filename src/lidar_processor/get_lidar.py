3d53ff63eef67e2b664c577e5f950cade2fc22d9
Prateek Humane <prateek.humane@gmail.com>
2019-01-07 17:00:53 -0500

    cleanup and add log messages
    use ros.loginfo instead of print

--- src/lidar_processor/get_lidar.py
+++ src/lidar_processor/get_lidar.py
@@ -6,26 +6,28 @@ from sensor_msgs.msg import LaserScan
 sd = None
 
 def callback(msg):
-	print('lidar front')
-	print(msg.ranges[0])
-	print('min lidar index and distance')
+	rospy.loginfo('lidar front')
+	rospy.loginfo(msg.ranges[0])
+	rospy.loginfo('min lidar index and distance')
 	index = 0
 	m = max(msg.ranges) # Minimum
 	for i in range(len(msg.ranges)):
 		if msg.ranges[i] < m and msg.ranges[i] > 0:
 			m = msg.ranges[i]
 			index = i
-	print('{}:\t {}'.format(index,m))
+	rospy.loginfo('{}:\t {}'.format(index,m))
 	sd.putNumber('testnum',index)
 
 def getLidarData():
 	global sd
+	rospy.loginfo('Initializing network tables')
 	NetworkTables.initialize(server='10.0.41.62')
 	sd = NetworkTables.getTable("SmartDashboard")
 	rospy.Subscriber('scan',LaserScan,callback)
 
 def main():
 	rospy.init_node('lidar_processor')
+	rospy.loginfo('starting lidar processor')
 	getLidarData()
 	rospy.spin()
 
