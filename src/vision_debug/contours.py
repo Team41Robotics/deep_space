73ab869a127aaaa1ad9d673b2056f324c599c4d5
Timtopia <timothyzhang6@gmail.com>
2019-01-11 15:51:21 -0500

    Comments

--- src/vision_debug/contours.py
+++ src/vision_debug/contours.py
@@ -1,12 +1,22 @@
 import cv2
 import numpy as np
 
-# Process for vision:
 # 1) Get video feed
 # 2) blur?
 # 3) Apply grayscale
 # 4) Threshold to black and white
 # 5) Find contours
+# 6) Filter contours by:
+#       - Width height ratio
+#       - Minimum area
+#       - Angle rotation
+#
+# 7)
+
+DEGREE_TOLERANCE = 7
+OPTIMAL_WH_RATIO = 4.0/11.0
+WH_TOLERANCE = .30
+AREA_TOLERANCE = 1000
 
 cap = cv2.VideoCapture(1)
 
@@ -14,43 +24,68 @@ cap = cv2.VideoCapture(1)
 f = open('webcam_config.txt', 'r')
 settings = f.readlines()
 f.close()
+
 for line in settings:
     prop, val = line.split('=')
     cap.set(getattr(cv2, prop), float(val))
 
+
 while True:
 
     ret, frame = cap.read()
+    tapeFrame = frame
 
-    # Create black image
-    blank = np.zeros((300, 300, 3), np.uint8)
-
+    # Blur image to reduce noise
     blur = cv2.GaussianBlur(frame, (9,9), 0)
 
+    # Convert image to greyscale
     gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('gray', gray)
+    #cv2.imshow('gray', gray)
 
+    # Convert image to black and white
     ret, thresh = cv2.threshold(gray, 175, 255, 0)
-    #ret, thresh = cv2.threshold(gray, 230, 255, 0)
-    cv2.imshow('thresh', thresh)
+    #cv2.imshow('thresh', thresh)
 
+    # Find contours from black and white image
     im2, contours, hierarchy = cv2.findContours(thresh,
                                                 cv2.RETR_TREE,
                                                 cv2.CHAIN_APPROX_SIMPLE)
 
-    cv2.drawContours(frame, contours, -1, (0,255,0), 3)
-    cv2.imshow('frame', frame)
-
+    # Check every contour
     for contour in contours:
+
+        # Draw bounding box around contour. This provides height, width, center,
+        # and rotation information.
         rect = cv2.minAreaRect(contour)
         box = cv2.boxPoints(rect)
         box = np.int0(box)
-        cv2.drawContours(frame, [box], 0, (0,0,255),2)
-    cv2.imshow('rectangle', frame)
         
+        width = min(rect[1])
+        height = max(rect[1])
+        rotation = rect[2]
+
+        # Check for width height ratio and 
+        if(rect[1][1]!=0 and
+           abs((width/height)-OPTIMAL_WH_RATIO)/(OPTIMAL_WH_RATIO)< WH_TOLERANCE and
+           width*height > AREA_TOLERANCE):
+
+            cv2.drawContours(frame, [box], 0, (0,0,255),2)
+
+            # Check if bounding boxes have correct angle of rotation
+            if abs(abs(rotation) - 75) < DEGREE_TOLERANCE:
+                cv2.drawContours(tapeFrame, [box], 0, (255,255,0),2)
+            if abs(abs(rotation) - 5) < DEGREE_TOLERANCE:
+                cv2.drawContours(tapeFrame, [box], 0, (0,255,255),2)
+
+    # Display frames            
+    cv2.imshow('rectangle', frame)
+    cv2.imshow('tape detection', tapeFrame)
+
+    # Take a pic and save to template.png
     if cv2.waitKey(1) == ord('c'):
         cv2.imwrite('template.png', frame)
-    
+
+    # Exit
     if cv2.waitKey(1) == ord('q'):
         break
     
