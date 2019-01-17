13dcd0181f49b21dac2f080db9ee781cba431600
Prateek Humane <prateek.humane@gmail.com>
2019-01-16 19:06:40 -0500

    added distance_estimation.py
    distance_estimation.py contains the class for estimating the distance from the vision target
    this is done using camera calibration, an overdetermined system and some math
    TODO: comment the estimator and combine the x and y of the pixel coordinates to one matrix

---
+++ src/vision_debug/distance_estimation.py
@@ -0,0 +1,45 @@
+import numpy as np
+import math
+
+class CameraEstimator: 
+	# it is assumed that x and y are already normalized
+	
+	def __init__(self, ref_x, ref_y, _view_angle_x, _view_angle_y, _cal_dist, _cal_span_x, _cal_span_y): 
+		self.view_angle_x = _view_angle_x*math.pi/180
+		self.view_angle_y = _view_angle_y*math.pi/180
+		self.cal_dist = _cal_dist
+		self.cal_span_x = np.matrix(_cal_span_x).T
+		self.cal_span_y = np.matrix(_cal_span_y).T
+		self.Ax = np.matrix(np.column_stack([ref_x,-np.ones(len(ref_x))]))
+		self.Ay = np.matrix(np.column_stack([ref_y,-np.ones(len(ref_y))]))
+		# calculate the least mean squared coefficients given system Ax=b
+		# where b is observed points normalized and x is sigma
+		# calculating the coefficient A beforehand so it need not be calculated each iteration
+		self.Alsx = (self.Ax.T*self.Ax).I*self.Ax.T	
+		self.Alsy = (self.Ay.T*self.Ay).I*self.Ay.T	
+
+		# using one matrix for x and y:
+		#self.A = np.matrix((Ax,Ay))
+		#self.Alms = (self.A.T*self.A).I*self.A.T
+
+	def get_distance(self, observed_points):
+		observed_points = np.matrix(observed_points)
+		soltn_x = self.Alsx*observed_points[:,0]
+		soltn_y = self.Alsy*observed_points[:,1]
+		sigma_dx = np.asscalar(soltn_x[0])
+		sigma_dy = np.asscalar(soltn_y[0])
+		tau_x = np.asscalar(soltn_x[1]/sigma_dx)
+		tau_y = np.asscalar(soltn_y[1]/sigma_dy)
+		ttx = math.tan(self.view_angle_x/2)
+		tty = math.tan(self.view_angle_y/2)
+		dist_est_x = self.cal_dist + self.cal_span_x / (2 * ttx) * (1 - sigma_dx) / sigma_dx
+		dist_est_y = self.cal_dist + self.cal_span_y / (2 * tty) * (1 - sigma_dy) / sigma_dy
+
+		dist = np.asscalar((dist_est_x+dist_est_y)/2)
+		x = np.asscalar(self.cal_span_x/2*tau_x)
+		y = np.asscalar(self.cal_span_y/2*tau_y)
+
+		return [dist,x,y]
+
+estimator = CameraEstimator([.1,.2,.3],[.4,.5,.6], 55, 77, 2, 4,10)
+print(estimator.get_distance([[.2,.4],[.3,.5],[.4,.6]]))
