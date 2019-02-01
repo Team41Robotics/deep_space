ca50c2c68b06340dc37e225e6f5d1193d326bd3a
Prateek Humane <prateek.humane@gmail.com>
2019-01-31 19:26:12 -0500

    Added Gstreamer script to services Updated installation script Gstreamer is currently using temporary cameras, to be changed

--- systemd/install.sh
+++ systemd/install.sh
@@ -14,4 +14,23 @@ cp ./files/gstreamer.sh /usr/local/bin/
 cp ./files/gstreamer.service /etc/systemd/system/
 echo "  Installed gstreamer.service"
 
+echo "Refreshing systemctl daemon..."
+systemctl daemon-reload
+
+echo "Starting services..."
+systemctl restart roscore
+echo "  Started roscore.service"
+systemctl restart deep_space
+echo "  Started deep_space.service"
+systemctl restart gstreamer
+echo "  Started gstreamer.service"
+
+echo "Enabling services on reboot..."
+systemctl enable roscore
+echo "  Enabled roscore.service"
+systemctl enable deep_space
+echo "  Enabled deep_space.service"
+systemctl enable gstreamer
+echo "  Enabled gstreamer.service"
+
 echo "Done."
